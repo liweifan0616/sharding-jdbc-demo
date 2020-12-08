6927b6a1f9ce134239bfa6e7695062f39120bc88
liweifan <592165246@qq.com>
2020-12-08 14:59:50 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/lwf/example/sharding/service/OrderService.java
@@ -0,0 +1,23 @@
+package com.lwf.example.sharding.service;
+
+import com.lwf.example.sharding.entity.OrderEntity;
+import com.lwf.example.sharding.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author ChengJianSheng
+ * @date 2020-06-18
+ */
+@Service
+public class OrderService {
+
+    @Resource
+    private OrderRepository orderRepository;
+
+    public void save(OrderEntity entity) {
+        orderRepository.save(entity);
+    }
+
+}
